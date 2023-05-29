e2d0953affe9473886960998d9e5a6ca3543fca2
Jeandre Visser <jeandre@nutrimeals.ca>
2023-05-28 22:11:42 -0600

    Create the feed, prompt list, and prompt card components

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,54 @@
+'use client';
+
 import React from 'react';
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='prompt_layout mt-16'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={posts} handleSearchChange={handleSearchChange} />
+    </section>
+  );
 };
 
 export default Feed;
