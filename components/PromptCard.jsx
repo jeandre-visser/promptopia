c11ede10cf35aba1daab95e971a998d241720075
Jeandre Visser <jeandre@nutrimeals.ca>
2023-05-30 21:54:26 -0600

    Update

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,15 +5,14 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const pathname = usePathname();
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
+  const pathName = usePathname();
   const router = useRouter();
-  const [copied, setCopied] = useState(false);
+
+  const [copied, setCopied] = useState('');
 
   const handleProfileClick = () => {
-    console.log(post);
-
     if (post.creator._id === session?.user.id) return router.push('/profile');
 
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
@@ -22,14 +21,14 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(''), 3000);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
         <div
-          className='flex-1 flex justify-start items-cetner gap-3 cursor-pointer'
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
           onClick={handleProfileClick}
         >
           <Image
@@ -39,11 +38,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className='rounded-full object-contain'
           />
+
           <div className='flex flex-col'>
             <h3 className='font-satoshi font-semibold text-gray-900'>
               {post.creator.username}
             </h3>
-            <p className='font-inter text-gray-500 text-sm'>
+            <p className='font-inter text-sm text-gray-500'>
               {post.creator.email}
             </p>
           </div>
@@ -56,7 +56,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 ? '/assets/icons/tick.svg'
                 : '/assets/icons/copy.svg'
             }
-            alt='copy'
+            alt={copied === post.prompt ? 'tick_icon' : 'copy_icon'}
             width={12}
             height={12}
           />
@@ -70,7 +70,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
-      {session?.user.id === post.creator._id && pathname === '/profile' && (
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
             className='font-inter text-sm green_gradient cursor-pointer'
